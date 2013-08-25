80f7bf72df07124be3a611f7c0785bf2025b0df0
Quentin Gibert <freezeeedos@gmail.com>
2013-08-25 22:19:25 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -Wall -g -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -o dlmanager dlmanager.c -lcurl
+gcc -Wall -Werror -g -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -o dlmanager dlmanager.c -lcurl
